b8117870226538d276518614f603372268c862cd
skilic1996 <safa.kilic96@gmail.com>
2020-06-03 22:13:16 +0200

    Feature(Test)

---
+++ src/_tests_/GameInfo.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {cleanup, render} from '@testing-library/react';
+import GameInfo from '../components/GameInfo';
+
+afterEach(cleanup);
+
+it('GameInfo Component Check Text in Div is Present', () => {
+    const {getByTestId } = render(
+      <GameInfo gameState="stale" labelOff="unkown" />,
+    );
+  
+    expect(getByTestId(/player1/i).textContent).toBe("Player 1");
+
+  });
--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -5,8 +5,8 @@ const GameInfo = ({ gameState = "stale", currentPlayer = "unkown" }) => (
   <React.Fragment>
     <h3 style={{float:"left"}}>It's your turn <span style={currentPlayer === "Player 1" ? styles.blue: styles.green}>{currentPlayer}</span></h3>
     <div style={{textAlign: "right"}}>
-      <h4 style={styles.blue}>Player 1</h4>
-      <h4 style={styles.green}>Player 2</h4>
+      <h4 data-testid="player1" style={styles.blue}>Player 1</h4>
+      <h4 data-testid="player2" style={styles.green}>Player 2</h4>
     </div>
   </React.Fragment>
 );
